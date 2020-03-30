b46b92b1130975a0efcfac721114abedc88a059d
budgidiere <sgidiere@gmail.com>
2020-03-30 10:14:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def getuseritems():
     while exit == False:
         tmpvar1 = input("Please enter number: ")
         if tmpvar1 == "done".lower():
-            exit = False
+            exit = True
             print("Checking")
         elif tmpvar1.isdigit():
             list46.append(tmpvar1)
@@ -18,5 +18,33 @@ def checkitems(inputlist, outputlist):
     for item in inputlist:
         is4 = False
         is6 = False
-        if item[-1] == "0" or item[-1] == "2" or item[-1] == "4" or item[-1] == "6" or item[-1] == "8"
-         last2 = (item[-1] + item[-2]
+        if (float(item[-1]) / 2).is_integer():
+            #print(2)
+            if (float(item[-1]) / 4).is_integer():
+                #print(4)
+                is4 = True
+            if (float(item) / 3).is_integer():
+                #print(6)
+                is6 = True
+            if is4 == True and is6 == True:
+                outputlist.append(True)
+            else:
+                outputlist.append(False)
+        else:
+            outputlist.append(False)
+    return(outputlist)
+
+def outputfunction(inputlist, outputlist):
+    i = 0
+    while i != len(inputlist):
+        if outputlist[i] == True:
+            print("{} is divisible by 4 and 6!".format(inputlist[i]))
+        else:
+            print("{} is not divisible by 4 and 6!".format(inputlist[i]))
+        i += 1
+
+
+getuseritems()
+checked46 = checkitems(list46, checked46)
+#print(checked46)
+outputfunction(list46, checked46)
